4b71f537bd0c233ea92423391243b868c03aded0
iivanov2 <iivanov2@yahoo.com>
2020-11-27 18:23:51 +0200

    notify_on_publish option added

--- config/translation-manager.php
+++ config/translation-manager.php
@@ -14,7 +14,17 @@ return [
         'prefix'     => 'translations',
         'middleware' => 'auth',
     ],
-
+    
+    /**
+     * If enabled=true notify admin when translations are published (because git commit and push could be needed).
+     * You need to implement your own global function simple_email(['to'=>'john@smith.com','body'=>'...']) to use this options, otherwise set it false
+     */
+    'notify_on_publish' => [
+        'enabled' => false,
+        'emails' => ['admin1@example.com','admin2@example.com'],
+        'email_text' => 'New translations were published. Git commit and push needed.'
+    ],
+    
     /**
      * Exclude specific groups from Laravel Translation Manager.
      * This is useful if, for example, you want to avoid editing the official Laravel language files.
